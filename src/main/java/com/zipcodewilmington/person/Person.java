54d0fd54a013b97c3a146b0e216e6e21bef57a61
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-11-02 08:59:24 -0400

    I have added in fields and have tested the methods all have been passed

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,92 @@ public class Person {
     private String name;
     private int age;
 
+    private String gender;
+
+    private int phone;
+
+    private String city;
+
+    private String state;
+
+    private int id;
+
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
+        gender = "";
+        phone = Integer.MAX_VALUE;
+        city = "";
+        state = "";
+        id = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+            return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,16 +12,31 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
-
+        String expectedGender = "";
+        Integer expectedPhone = Integer.MAX_VALUE;
+        String expectedCity = "";
+        String expectedState = "";
+        Integer expectedId = Integer.MAX_VALUE;
         // When
         Person person = new Person();
 
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        String actualGender = person.getGender();
+        Integer actualPhone = person.getPhone();
+        String actualCity = person.getCity();
+        String actualState = person.getState();
+        Integer actualId = person.getId();
+
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedPhone, actualPhone);
+        Assert.assertEquals(expectedCity, actualCity);
+        Assert.assertEquals(expectedState, actualState);
+        Assert.assertEquals(expectedId, actualId);
     }
 
     @Test
@@ -93,6 +108,75 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetGender() {
+        //Given
+        Person person = new Person();
+        String expected = "male";
+
+        //When
+        person.setGender(expected);
+        String actual = person.getGender();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetPhone() {
+        //Given
+        Person person = new Person();
+        Integer expected = 843-632-4328;
+
+        //When
+        person.setPhone(expected);
+        Integer actual = person.getPhone();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetCity() {
+        //Given
+        Person person = new Person();
+        String expected = "Manhattan";
+
+        //When
+        person.setCity(expected);
+        String actual = person.getCity();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetState() {
+        //Given
+        Person person = new Person();
+        String expected = "New York";
+
+        //When
+        person.setState(expected);
+        String actual = person.getState();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetId() {
+        //Given
+        Person person = new Person();
+        Integer expected = 234563432;
+
+        //When
+        person.setId(expected);
+        Integer actual = person.getId();
+
+        //Then
         Assert.assertEquals(expected, actual);
     }
 }
